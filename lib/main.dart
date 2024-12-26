dda96f7b65580b76f2062d56b32c8a23168cffd5
vearne <asdwoshiaotian@gmail.com>
2024-12-26 11:01:51 +0800

    支持otp

--- lib/main.dart
+++ lib/main.dart
@@ -10,6 +10,13 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Password Manager',
+      routes: {
+        '/login': (context) => LoginScreen(),
+        // '/passwordList': (context) => PasswordListScreen(
+        //   username: 'defaultUsername', // 这里需要动态传入
+        //   secureHash: 'defaultSecureHash', // 这里需要动态传入
+        // ),
+      },
       theme: ThemeData(primarySwatch: Colors.blue),
       home: LoginScreen(), // 启动时显示登录界面
     );
